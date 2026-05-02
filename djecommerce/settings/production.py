46a92c54712192edec1747cb10f2461c32feffb1
angelodirb <168436547+angelodirb@users.noreply.github.com>
2026-05-02 12:06:42 -0500

    Update production settings for database options
    Add database options for timezone configuration.

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -18,7 +18,10 @@ DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
         conn_max_age=600
-    )
+    ),
+    'OPTIONS': {
+            'options': '-c timezone=UTC'
+    }
 }
 
 # WhiteNoise para archivos estáticos
